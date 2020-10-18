6115f02306978baa82b33200db6ad0df174a67be
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2020-10-18 17:18:33 +0300

    yay

--- Q3J.py
+++ Q3J.py
@@ -4,64 +4,72 @@ def getSteps(W, N, x):
     #implement
     return counter
 
-def binarySearchWithBranch(W, N, low, high, branchInLeft):
+def binarySearchWithBranch(W, N, low, high, branchInLeft, startValue):
     #private
     mid = (low + high) / 2
+    x = getSteps(W, N, mid)
 
     while low < high:
-        x = getSteps(W, N, mid)
-
         if mid == 0:
             x_u = getSteps(W, N, 1)
             if x_u < x:
-                return 1
+                return x_u
             else:
-                return 0
-        if mid == N-1:
-            x_l = getSteps(W, N, N - 2)
+                return x
+        if mid == N:
+            x_l = getSteps(W, N, N - 1)
             if x_l < x:
-                return N-2
+                return x_l
             else:
-                return N-1
+                return x
 
         x_l = getSteps(W, N, mid-1)
         x_u = getSteps(W, N, mid+1)
 
         if x_l >= x and x_u >= x:
-            return mid
+            return x
 
         if x_l < x:
-            high = mid
+            if branchInLeft and startValue < x:
+                low = mid
+            else:
+                high = mid
         elif x_u < x:
-            low = mid
+            if not branchInLeft and startValue < x:
+                high = mid
+            else:
+                low = mid
 
         mid = (low + high) / 2
+        x = getSteps(W, N, mid)
+
+    return x
 
 def cyclicBinarySearch(W, N):
     low = 2
     high = N
     mid = (low+high)/2
     startAt = mid
+    startValue = getSteps(W, N, startAt)
+    x = getSteps(W, N, mid)
 
     while low < high:
-        x = getSteps(W, N, mid)
-
         if mid == 2:
             if x > getSteps(W, N, N):
-                return binarySearchWithBranch(W, N, startAt, N, True)
+                return binarySearchWithBranch(W, N, startAt, N, True, startValue)
             else:
-                return mid
+                return x
         if mid == N:
             if x > getSteps(W, N, 2):
-                return binarySearchWithBranch(W, N, 2 ,startAt, False)
+                return binarySearchWithBranch(W, N, 2 ,startAt, False, startValue)
             else:
-                return mid
+                return x
 
         x_l = getSteps(W, N, mid-1)
         x_u = getSteps(W, N, mid+1)
 
         if x_l >= x and x_u >= x:
-            return mid
+            return x
 
         if x_l < x:
             high = mid
@@ -69,5 +77,6 @@ def cyclicBinarySearch(W, N):
             low = mid
 
         mid = (low + high) / 2
+        x = getSteps(W, N, mid)
 
-    return mid
+    return x
